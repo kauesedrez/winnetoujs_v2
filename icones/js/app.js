ce1e70eedd2d66753bdbfad38ce645239a30d8d6
Kaue Pavillion LINUX <kaue.sedrez@gmail.com>
2020-08-10 16:47:17 -0300

    antes de tentar o create dentro do constructo

--- icones/js/app.js
+++ icones/js/app.js
@@ -3,22 +3,3 @@ import { Constructos, Winnetou } from "../winnetou.js";
 let div1 = Constructos.divSimples({ texto: "Olá Ícones" });
 
 Winnetou.create(div1.code, "#app");
-
-// ícone simples
-Winnetou.create(Constructos.icons_delete().code, "#app");
-
-// ícone dual tone
-Winnetou.create(
-  Constructos.icons_compass({ class: "dualTone" }).code,
-  "#app"
-);
-
-// ícone colorido
-Winnetou.create(Constructos.coloredIcons_camera().code, "#app");
-
-// ícone colorido
-Winnetou.create(Constructos.coloredIcons_tropical().code, "#app");
-
-// ícone colorido colocado na pasta de ícones normais
-// [!] NÃO FAÇA ISSO
-Winnetou.create(Constructos.icons_tropical().code, "#app");
